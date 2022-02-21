d3f0dba51c2d211a080d0a984b15e474fea78e45
Rob <robzlegz@gmail.com>
2022-02-21 18:19:06 +0200

    cmptnts

---
+++ src/components/ProfileContainer.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+import styled from "styled-components/native";
+
+export default function ProfileContainer() {
+  return (
+    <StyledProfileContainer>
+    </StyledProfileContainer>
+  )
+}
+
+const StyledProfileContainer = styled.View`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
